5151d58c12083f72a002b75327fcb9632283be82
Rob Glazebrook <rglazebrook@gmail.com>
2014-11-15 17:38:37 -0600

    Adding sketch.js to the mix and adding a background canvas.

--- wp-content/themes/awesome_hueman/functions.php
+++ wp-content/themes/awesome_hueman/functions.php
@@ -1,6 +1,14 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' );
-function enqueue_parent_theme_style() {
+add_action( 'wp_enqueue_scripts', 'enqueue_awesome_scripts' );
+
+function enqueue_awesome_scripts() {
+	$ss = get_stylesheet_directory_uri();
+
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+
+    wp_enqueue_script('sketchjs', $ss . '/js/sketch.min.js', array(), false, true);
+    wp_enqueue_script('awesome-main', $ss . '/js/main.js', array('sketchjs'), false, true);
 }
+
+?>
---
+++ wp-content/themes/awesome_hueman/js/main.js
@@ -0,0 +1,6 @@
+(function($){ 
+
+	// Create the sketch background canvas.
+	var canvasbg = new Sketch.create({ container: $('#canvasbg')[0] });
+
+})(jQuery);
